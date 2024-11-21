61ffa34046917a7f0c1ddab247287452a7ec4b4a
Danilo Filgueira <danilo@origemmotos.com.br>
2024-11-21 17:23:42 -0300

    chore: rubocop

--- lib/main.rb
+++ lib/main.rb
@@ -1,16 +1,21 @@
-require "zeitwerk"
+# frozen_string_literal: true
+
+require 'zeitwerk'
+
+require 'pry'
+require 'pry-byebug'
 
 # Auto load all files in the project lib directory
 loader = Zeitwerk::Loader.new
-loader.ignore("lib/main.rb")
-loader.push_dir("lib/app/controllers")
-loader.push_dir("lib/app/parsers")
-loader.push_dir("lib/app/services")
-loader.push_dir("lib/app/repositories")
-loader.push_dir("lib/app/validators")
-loader.push_dir("lib/domain/entities")
-loader.push_dir("lib/utils")
-loader.inflector.inflect "cli" => "CLI"
+loader.ignore('lib/main.rb')
+loader.push_dir('lib/app/controllers')
+loader.push_dir('lib/app/parsers')
+loader.push_dir('lib/app/services')
+loader.push_dir('lib/app/repositories')
+loader.push_dir('lib/app/validators')
+loader.push_dir('lib/domain/entities')
+loader.push_dir('lib/utils')
+loader.inflector.inflect 'cli' => 'CLI'
 loader.setup # ready!
 
 begin
