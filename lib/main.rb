38087318d8038e80f236373db1a38b70d3174c74
Danilo Filgueira <danilo@origemmotos.com.br>
2024-11-23 18:50:00 -0300

    ref: move repositories to the lib root

--- lib/main.rb
+++ lib/main.rb
@@ -13,9 +13,9 @@ loader.ignore('lib/main.rb')
 loader.push_dir('lib/app/controllers')
 loader.push_dir('lib/app/parsers')
 loader.push_dir('lib/app/services')
-loader.push_dir('lib/app/repositories')
 loader.push_dir('lib/app/validators')
 loader.push_dir('lib/domain/entities')
+loader.push_dir('lib/repositories')
 loader.push_dir('lib/utils')
 loader.setup # ready!
 
