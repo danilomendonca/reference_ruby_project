dbbef688ca7d735e2dd644cb38358e40b5c476aa
Danilo Filgueira <danilo@origemmotos.com.br>
2024-11-25 18:43:28 -0300

    feat: add a run_cli.sh script to make it easier to run the containerized app

---
+++ scripts/run_cli.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Script to run a command inside the project container
+
+# Check if at least two arguments are passed
+if [ $# -lt 2 ]; then
+  echo "Usage: ./run_cli.sh <command> <arg>"
+  exit 1
+fi
+
+# Extract the command and argument
+COMMAND=$1
+ARG=$2
+
+# Execute the container command
+docker compose run --rm project bundle exec ruby bin/cli "$COMMAND" "$ARG"
